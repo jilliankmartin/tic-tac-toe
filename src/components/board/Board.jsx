7d0d5b302246e1e732dae57008519408f33f1797
Jillian Martin <jillmartin@live.com>
2022-12-18 15:34:13 -0800

    add gameplay

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Board.css';
 import Square from './square/Square';
 
 const Board = () => {
-  const greeting = 'Board Component!';
-  const squares = [
-    {id: 1},
-    {id: 2},
-    {id: 3},
-    {id: 4},
-    {id: 5},
-    {id: 6},
-    {id: 7},
-    {id: 8},
-    {id: 9},
+  const defaultSquaresState = [
+    {id: 1, value: ""},
+    {id: 2, value: ""},
+    {id: 3, value: ""},
+    {id: 4, value: ""},
+    {id: 5, value: ""},
+    {id: 6, value: ""},
+    {id: 7, value: ""},
+    {id: 8, value: ""},
+    {id: 9, value: ""},
   ];
+  const [squares, setSquares] = useState(defaultSquaresState) 
+  const [round, setRound] = useState(0)
+  const [computersTurn, setComputersTurn] = useState(false)
+
+  useEffect(() => {
+    if (round !== 0 && computersTurn) {
+      setTimeout(() => {
+        const emptySquares = squares.filter((square) => square.value === "")
+        const randomSquare = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+        setSquares(squares.map((square) => {
+          if (square.id === randomSquare.id) {
+            return {
+              ...square,
+              value: "o"
+            }
+          }
+          return square;
+        }))
+        setComputersTurn(false)
+      }, 2000)
+    }
+  }, [squares])
 
   return (
     <div class="board">
       {squares.map((square) => {
-        console.log(square.id)
-        return (<Square classIndex={square.id} />)
+        return (<Square classIndex={square.id} value={square.value} squares={squares} setSquares={setSquares} round={round} setRound={setRound} setComputersTurn={setComputersTurn}/>)
       })}
     </div>
     
--- src/components/board/square/Square.jsx
+++ src/components/board/square/Square.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './Square.css';
 
-const Square = ({classIndex}) => {
-  const greeting = 'Square Component!';
+const Square = ({classIndex, value, squares, setSquares, round, setRound, setComputersTurn}) => {
+  const onClickHandler = () => {
+    setSquares(
+      squares.map((square) => {
+        if (square.id === classIndex) {
+          return {
+            ...square,
+            value: "x",
+          }
+        }
+        return square;
+      })
+    );
+    setRound(round + 1)
+    setComputersTurn(true)
+  }
 
-  return <div className={`square ref-${classIndex}`}>{greeting}</div>;
+  return <div className={`square ref-${classIndex}`} onClick={onClickHandler}>{value}</div>;
 };
 
 export default Square;
